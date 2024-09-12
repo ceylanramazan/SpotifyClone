86226b77603df4b1579e1e10f6ff3406a2924998
Ramazan CEYLAN <ramazan.ceylan2@halkbank.com.tr>
2024-09-12 12:59:56 +0300

    added - modules

---
+++ Modules/Player/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Player",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Player",
+            targets: ["Player"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Player"),
+        .testTarget(
+            name: "PlayerTests",
+            dependencies: ["Player"]),
+    ]
+)
