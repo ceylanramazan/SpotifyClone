393290713f442c101b620ba62d234fc0b5ae782b
Ramazan CEYLAN <ramazan.ceylan2@halkbank.com.tr>
2024-09-19 19:53:30 +0300

    added - Defined Splash module with dependencies on Common, Network, and AppResources
    - Added a new `Package.swift` for the Splash module.
    - Set up dependencies for Common, Network, and AppResources modules.
    - Configured Splash module with test target `SplashTests`.

--- Modules/Splash/Package.swift
+++ Modules/Splash/Package.swift
@@ -1,23 +1,33 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// Splash/Package.swift
 import PackageDescription
 
 let package = Package(
-    name: "Splash",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Splash",
-            targets: ["Splash"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Splash"),
-        .testTarget(
-            name: "SplashTests",
-            dependencies: ["Splash"]),
-    ]
+  name: "Splash",
+  products: [
+    .library(
+      name: "Splash",
+      targets: ["Splash"]),
+  ],
+  dependencies: [
+    .package(path: "../Common"),
+    .package(path: "../Network"),
+    .package(path: "../AppResources")
+  ],
+  targets: [
+    .target(
+      name: "Splash",
+      dependencies: [
+        "Common",
+        "Network",
+        "AppResources"
+      ]
+    ),
+    .testTarget(
+      name: "SplashTests",
+      dependencies: ["Splash"]),
+  ]
 )
+
