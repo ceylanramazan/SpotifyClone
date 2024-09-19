7492191cfbcc5e433955340a34c13332fcab27cf
Ramazan CEYLAN <ramazan.ceylan2@halkbank.com.tr>
2024-09-19 19:46:25 +0300

    added - AssetManager struct to centralize app assets management
    - Introduced a new struct `AssetManager` to centralize asset retrieval.
    - Added a static function `appImage` to access UIImage assets using the `AppImages` enum from the UIImage extension.

---
+++ Modules/AppResources/Sources/AppResources/Utils/AssetManager.swift
@@ -0,0 +1,16 @@
+//
+//  AssetManager.swift
+//
+//
+//  Created by Ramazan Ceylan on 13.09.2024.
+//
+
+import UIKit
+
+public struct AssetManager {
+
+    // MARK: - Images (using Enum)
+    public static func appImage(_ image: UIImage.AppImages) -> UIImage? {
+        return UIImage.appImage(image)
+    }
+}
