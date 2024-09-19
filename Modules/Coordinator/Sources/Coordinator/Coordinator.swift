e361f05a3f1da00ba70fa05262c292b3268dd457
Ramazan CEYLAN <ramazan.ceylan2@halkbank.com.tr>
2024-09-19 20:07:14 +0300

    added - Defined Coordinator protocol for navigation coordination
    - Introduced `Coordinator` protocol to define the structure for all coordinators.
    - Enforced the implementation of basic coordinator methods like `start()`, `addChild()`, and `removeChild()`.

--- Modules/Coordinator/Sources/Coordinator/Coordinator.swift
+++ Modules/Coordinator/Sources/Coordinator/Coordinator.swift
@@ -1,2 +1,22 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Coordinator.swift
+//
+//
+//  Created by Ramazan Ceylan on 12.09.2024.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+  var router: Router { get set }
+  var childCoordinators: [Coordinator] { get set }
+
+  // Start the coordinator
+  func start()
+
+  // Add a child coordinator
+  func addChild(_ coordinator: Coordinator)
+
+  // Remove a child coordinator
+  func removeChild(_ coordinator: Coordinator)
+}
